d6398f6ccc6986d909e676b337b0e379ffdb953a
VladislavKut <vladkutuzov19@gmail.com>
2022-06-28 23:21:59 +0500

    Переделал Биографию

--- add/biography.js
+++ add/biography.js
@@ -1,28 +1,9 @@
 const biographyCity = document.getElementById('biography__city');
 const cityHint = document.getElementById('city__hint');
-// var coord = biographyCity.getBoundingClientRect();
-
-
-
-// if (biographyCity.onmousemove == true) {
-//     biographyCity.onmousemove = function(event) {
-//         cityHint.style.display = 'block';
-//         cityHint.style.left = (event.clientX - coord.left) + 'px';
-//         cityHint.style.top = (event.clientY - coord.top) + 'px';
-//     }
-// } else {
-//     cityHint.style.display = 'none';
-// }
-
-// biographyCity.addEventListener("mouseout", function() {
-
-//     cityHint.style.display = 'none';
-
-// });
-
 
 biographyCity.onmousemove = (e) => {
     cityHint.style.opacity = '1';
+    // cityHint.style.transform = 'scaleX(-1)';
 
     const x = e.offsetX;
     const y = e.offsetY;
